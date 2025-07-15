e3dc0b8c1704c8712cba2dac08e352d90871e0ce
pedrazasurveying <jose@pedrazasurveying.com>
2025-07-15 10:46:18 -0500

    Update app.py

--- app.py
+++ app.py
@@ -187,5 +187,8 @@ def openapi_spec():
         }
     })
 
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
